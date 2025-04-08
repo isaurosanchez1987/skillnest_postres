e36b9b82908eb299febac3b264f922fec3a377c7
isaurosanchez1987 <isaurosancheztrader@gmail.com>
2025-04-08 15:02:35 -0500

    Generando la configuracion para home

--- postres-app/src/main/java/com/postres/postres_app/PostresAppApplication.java
+++ postres-app/src/main/java/com/postres/postres_app/PostresAppApplication.java
@@ -10,10 +10,29 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class PostresAppApplication  {
-
+public class PostresAppApplication  extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(PostresAppApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(PostresAppApplication.class);
+	}
+
+	@Configuration
+	public static class MyConfiguration {
+
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 }
